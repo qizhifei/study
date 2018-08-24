a4dc6bbb5b3b334a81fa3fbc2e3a9c0b572bd6b3
qizhifei <137867590@qq.com>
2018-08-24 08:31:32 +0000

    完成静态页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','about')
+@section('content')
+<p>关于页面</p>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','home')
+@section('content')
+<h2>HOME</h2>
+@stop
