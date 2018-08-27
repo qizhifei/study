72129a805af28fc01dcf47696946fe36f408a8b3
祁志飞 <137867590@qq.com>
2018-08-27 15:52:55 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('signup','UsersController@create')->name('signup');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
