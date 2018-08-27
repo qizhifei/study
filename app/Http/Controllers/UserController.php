fdd419927ab272d47578b66bc7db18f0b482021c
祁志飞 <137867590@qq.com>
2018-08-27 15:39:58 +0800

    1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('user.create');
+    }
+
+    public function show(User,$user)
+    {
+        return view('users.show',compact($user));
     }
 }
---
+++ app/Models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'password', 'password_confirmation'];
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+}
